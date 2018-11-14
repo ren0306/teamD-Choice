53aa48a3162db7086351fb58db1e40f8aa22df1b
ren0306 <h1201815026@hiratagakuen.onmicrosoft.com>
2018-11-14 10:46:52 +0900

    a

--- Choice-D/Choice-D/GameOver/GameOver2.cpp
+++ Choice-D/Choice-D/GameOver/GameOver2.cpp
@@ -29,6 +29,36 @@ CSceneGameOver2::~CSceneGameOver2()
 //ゲームメイン初期化メソッド
 void CSceneGameOver2::InitScene()
 {
+	//外部グラフィック画像を読み込み15番に登録
+	Draw::LoadImage(L"teki3.png", 15, TEX_SIZE_512);
+
+	CObjGameOvergamen* ga6 = new CObjGameOvergamen();
+	Objs::InsertObj(ga6, OBJ_GAMEOVERGAMEN, 24);
+
+	//外部グラフィック画像を読み込み14番に登録
+	Draw::LoadImage(L"teki3.png", 14, TEX_SIZE_512);
+
+	CObjGameOvergamen* ga5 = new CObjGameOvergamen();
+	Objs::InsertObj(ga5, OBJ_GAMEOVERGAMEN, 23);
+
+	//外部グラフィック画像を読み込み13番に登録
+	Draw::LoadImage(L"teki3.png", 13, TEX_SIZE_512);
+
+	CObjGameOvergamen* ga4 = new CObjGameOvergamen();
+	Objs::InsertObj(ga4, OBJ_GAMEOVERGAMEN, 22);
+
+	//外部グラフィック画像を読み込み12番に登録
+	Draw::LoadImage(L"teki3.png", 12, TEX_SIZE_512);
+
+	CObjGameOvergamen* ga3 = new CObjGameOvergamen();
+	Objs::InsertObj(ga3, OBJ_GAMEOVERGAMEN, 21);
+
+	//外部グラフィック画像を読み込み9番に登録
+	Draw::LoadImage(L"yarareta.png", 9, TEX_SIZE_512);
+
+	CObjGameOvergamen* ga2 = new CObjGameOvergamen();
+	Objs::InsertObj(ga2, OBJ_GAMEOVERGAMEN, 20);
+
 	//出力させる文字のグラフィックを作成
 	Font::SetStrTex(L"GAME OVER");
 
