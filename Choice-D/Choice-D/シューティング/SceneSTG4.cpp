1080b28655f51d1c2c62c5cd0349b0f8b82306b9
GA1 <GA1@DEV1-3>
2018-11-14 10:45:05 +0900

    SceneSTG4微バグ修正

--- Choice-D/Choice-D/シューティング/SceneSTG4.cpp
+++ Choice-D/Choice-D/シューティング/SceneSTG4.cpp
@@ -52,7 +52,7 @@ void CSceneSTG4::InitScene()
 	Objs::InsertObj(q, OBJ_BACKGROUND, 20);
 
 	CObjteki4* objb = new CObjteki4(200, 10);
-	Objs::InsertObj(objb, OBJ_TEKI1, 50);
+	Objs::InsertObj(objb, OBJ_TEKI4, 50);
 
 	CObjHero*ob = new CObjHero();
 	Objs::InsertObj(ob, OBJ_HERO, 150);
