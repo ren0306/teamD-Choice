f167c9091ff9bab56a0773bf3e5b5ea1730d99e8
ren0306 <h1201815026@hiratagakuen.onmicrosoft.com>
2018-11-21 10:11:51 +0900

    w

--- Choice-D/Choice-D/GameHead.h
+++ Choice-D/Choice-D/GameHead.h
@@ -161,5 +161,5 @@ struct UserData
 
 //シーンスタートクラス---------------------------
 //ゲーム開始時のシーンクラス登録
-#define SET_GAME_START CSceneMain4
+#define SET_GAME_START CSceneMain
 //-----------------------------------------------
