767decb5457257602360492cbf8cddc121632662
GA1 <GA1@DEV1-3>
2018-12-10 17:41:22 +0900

    Hero消滅エフェクト実行途中

--- Choice-D/Choice-D/シューティング/ObjHero.cpp
+++ Choice-D/Choice-D/シューティング/ObjHero.cpp
@@ -52,6 +52,15 @@ void CObjHero::Init()
 	m_maxtame = 100.f;
 	m_and = 1.f;
 	m_andf = true;
+	death = false;
+	size1 = 0.0f;
+	size2 = 0.0f;
+	m_eff.m_top = 0;
+	m_eff.m_left = 96;
+	m_eff.m_right = 128;
+	m_eff.m_bottom = 32;
+	m_ani = 0;
+	m_ani_time = 0;
 	//当たり判定用HitBoxを作成
 	Hits::SetHitBox(this, m_x, m_y, 32, 32, ELEMENT_PLAYER, OBJ_HERO, 1);
 }
@@ -59,6 +68,53 @@ void CObjHero::Init()
 //アクション
 void CObjHero::Action()
 {
+	if (death == true)
+	{
+		RECT_F ani_src[5] =
+		{
+			{ 0,  96, 128,  32 },
+			{ 64,  32,  64,  96 },
+			{ 96, 224, 256,  128 },
+			{ 160, 160, 192, 192 },
+			{ 224,  96, 128, 256 },
+		};
+
+		//アニメーションのコマ間隔制御
+		if (m_ani_time > 4)
+		{
+			m_ani++;	//アニメーションのコマを１つ進める
+			m_ani_time = 0;
+
+			m_eff = ani_src[m_ani];//アニメーションのRECT配列からm_ani番目のRECT情報取得
+		}
+		else
+		{
+			m_ani_time++;
+		}
+
+		//自機爆発アニメーション終了で本当にオブジェクトの破棄＆シーン遷移
+		if (m_ani == 5)
+		{
+			this->SetStatus(false);		//自身に削除命令を出す。
+			Hits::DeleteHitBox(this);	//主人公機が所有するHitBoxに削除する。
+
+			Scene::SetScene(new CSceneGameOver2());
+		}
+
+		/*
+		m_time--;
+		if (m_time <= 0)
+		{
+			this->SetStatus(false);		//自身に削除命令を出す。
+			Hits::DeleteHitBox(this);	//主人公機が所有するHitBoxに削除する。
+
+			Scene::SetScene(new CSceneGameOver2());
+		}
+		*/
+
+		return;//消滅処理は、ここでアクションメソッドを終了させる
+	}
+
 	float c[4] = { 1.0f,1.0f,1.0f,1.0f };
 	//フェードイン
 	if (m_andf == true)
@@ -244,15 +300,10 @@ void CObjHero::Action()
 	//HPが0になったら破棄
 	if (m_Mehp <= 0)
 	{
-		m_time--;
-		if (m_time <= 0)
-		{
-			this->SetStatus(false);		//自身に削除命令を出す。
-			Hits::DeleteHitBox(this);	//主人公機が所有するHitBoxに削除する。
+		death = true;
 
-
-			Scene::SetScene(new CSceneGameOver2());
-		}
+		//size1 = -20.0f;
+		//size2 = +20.0f;
 	}
 }
 
@@ -267,7 +318,7 @@ void CObjHero::Draw()
 		{ 32, 64,  96,  64},
 		{ 32, 96, 128,  64},
 	};
-	
+
 	//描画カラー情報  R=RED  G=Green  B=Blue A=alpha(透過情報)
 	float  ob[4] = { 1.0f,1.0f,1.0f,1.0f };
 
@@ -337,12 +388,12 @@ void CObjHero::Draw()
 	src.m_bottom = 32.0f;
 
 	//表示位置の設定
-	dst.m_top = 0.0f + m_y;
-	dst.m_left = 0.0f + m_x;
-	dst.m_right = 32.0f + m_x;
-	dst.m_bottom = 32.0f + m_y;
+	dst.m_top = 0.0f + size1 + m_y;
+	dst.m_left = 0.0f + size1 + m_x;
+	dst.m_right = 32.0f + size2 + m_x;
+	dst.m_bottom = 32.0f + size2 + m_y;
 
-	//1番目に登録したグラフィックをsrc・dst・cの情報を元に描画
+	//6番目に登録したグラフィックをsrc・dst・cの情報を元に描画
 	Draw::Draw(6, &src, &dst, ob, 0.0f);
 
 }
