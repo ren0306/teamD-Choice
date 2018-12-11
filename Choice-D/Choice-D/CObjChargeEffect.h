7c4cd335830e1742bc7731681a1008a587299730
GA1 <GA1@DEV1-3>
2018-12-11 16:14:36 +0900

    チャージエフェクト実装途中

---
+++ Choice-D/Choice-D/CObjChargeEffect.h
@@ -0,0 +1,22 @@
+#pragma once
+//使用するヘッダー
+#include"GameL\DrawTexture.h"
+#include"GameL\SceneObjManager.h"
+//使用するネームスペース
+using namespace GameL;
+
+//オブジェクト：チャージエフェクト
+class CObjChargeEffect : public CObj
+{
+public:
+	CObjChargeEffect(float x, float y);
+	~CObjChargeEffect() {};
+	void Init();	//イニシャライズ
+	void Action();  //アクション
+	void Draw();	//ドロー
+	void Set(int a);//アニメーションコマ変更用
+private:
+	int	   m_ani;	//着弾アニメーション用
+	int	   m_ani_time;//着弾アニメーション間隔タイム
+	RECT_F m_eff;	//着弾エフェクト描画用
+};
