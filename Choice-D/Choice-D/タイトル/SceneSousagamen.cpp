72902da2417f1bb9f9323c4d1b22821d3c1a6ba5
野山 <h1201719002@hiratagakuen.onmicrosoft.com>
2019-01-21 14:23:04 +0900

    Zz

--- Choice-D/Choice-D/タイトル/SceneSousagamen.cpp
+++ Choice-D/Choice-D/タイトル/SceneSousagamen.cpp
@@ -6,6 +6,7 @@
 #include "../GameL\SceneObjManager.h"
 #include "../GameL\DrawFont.h"
 #include "../GameL/DrawTexture.h"
+#include "../GameL/Audio.h"
 
 //使用するネームスペース
 using namespace GameL;
@@ -30,13 +31,16 @@ void CSceneSousagamen::InitScene()
 {
 	//出力させる文字のグラフィックを作成
 	Font::SetStrTex(L"ゲームを始める");
-	Draw::LoadImage(L"ki-bo-do.png", 0, TEX_SIZE_512);
-	Draw::LoadImage(L"mausu.png", 1, TEX_SIZE_512);
-	//Draw::LoadImage(L"tenbin.png", 1, TEX_SIZE_512);
+	Draw::LoadImage(L"ki-bo-do.png"	,0, TEX_SIZE_512);
+	Draw::LoadImage(L"mausu.png"	,1,TEX_SIZE_512);
+	Draw::LoadImage(L"tenbin.png"	,1, TEX_SIZE_512);
 
 	//操作画面オブジェクト作成
 	CObjSousagamen* s = new CObjSousagamen();
 	Objs::InsertObj(s, OBJ_SOUSAGAMEN, 10);
+
+	Audio::LoadAudio(0, L"titleBGM.wav", SOUND_TYPE::BACK_MUSIC);
+	Audio::Start(0);
 }
 
 //操作画面実行中メソッド
